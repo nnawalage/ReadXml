614f5510baa5901a0c4be61ebc71e5b945f7f7cb
Nilmi Nawalage <ndn@tiqri.com>
2018-11-02 10:30:45 +0530

    Add project files.

---
+++ ReadXml/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace ReadXml
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string currentDirectory = Directory.GetCurrentDirectory();
+            string filePath = Path.Combine(currentDirectory, "ResponseResultLocalization.xml");
+            XElement root = XElement.Load(filePath);
+            var node = root.Elements("Error").FirstOrDefault(el => el.Attribute("code").Value.Equals("110"));
+            if (node != null)
+            {
+                var englishMessage = node.Element("MessageEn").Value;
+                Console.WriteLine(englishMessage);
+            }
+
+            Console.Read();
+        }
+    }
+}
